441a2637827fc8d760b14ff40bd58a3be6f83c9c
programmershakib <programmershakibinfo@gmail.com>
2024-12-17 01:41:34 +0600

    added a jwt

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use(cookieParser());
+
+const logger = (req, res, next) => {
+  console.log("logger");
+  next();
+};
+
+const verifyToken = (req, res, next) => {
+  // console.log("inside verifyToken token", req.cookies);
+  const token = req.cookies?.token;
+  if (!token) {
+    return res.status(401).send({ message: "unAuthorized access" });
+  }
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "unAuthorized access" });
+    }
+    req.user = decoded;
+    next();
+  });
+};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@programmershakib.sm4uc.mongodb.net/?retryWrites=true&w=majority&appName=programmershakib`;
 
@@ -35,7 +64,22 @@ async function run() {
       .db("jobsData")
       .collection("job_applications");
 
-    app.get("/jobs", async (req, res) => {
+    // auth related api
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "5h",
+      });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: false,
+        })
+        .send({ success: true });
+    });
+
+    app.get("/jobs", logger, async (req, res) => {
+      console.log("inside logger");
       const email = req.query.email;
       let query = {};
       if (email) {
@@ -60,9 +104,12 @@ async function run() {
     });
 
     // job application
-    app.get("/job-applications", async (req, res) => {
+    app.get("/job-applications", verifyToken, async (req, res) => {
       const email = req.query.email;
       const query = { applicant_email: email };
+      if (req.user.email !== req.query.email) {
+        return res.status(403).send({ message: "Forbidden access" });
+      }
       const result = await jobApplicationCollection.find(query).toArray();
       // fokira
       for (const application of result) {
@@ -79,7 +126,7 @@ async function run() {
     });
 
     app.get("/job-applications/jobs/:job_id", async (req, res) => {
-      const jobId = req.params.id;
+      const jobId = req.params.job_id;
       const query = { job_id: jobId };
       const result = await jobApplicationCollection.find(query).toArray();
       res.send(result);
@@ -104,6 +151,22 @@ async function run() {
         },
       };
       const updatedResult = await jobsCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
+
+    app.patch("/job-applications/:id", async (req, res) => {
+      const id = req.params.id;
+      const data = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          status: data.status,
+        },
+      };
+      const result = await jobApplicationCollection.updateOne(
+        filter,
+        updatedDoc
+      );
       res.send(result);
     });
 
